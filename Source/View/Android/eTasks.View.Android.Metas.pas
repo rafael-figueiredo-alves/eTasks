bbdac35eb60192aceb89858139127440cc449c97
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2021-06-13 19:42:18 -0300

    Update #1 - 13/06/2021

--- Source/View/Android/eTasks.View.Android.Metas.pas
+++ Source/View/Android/eTasks.View.Android.Metas.pas
@@ -7,7 +7,8 @@ uses
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Layouts,
   FMX.Ani, FMX.Objects, FMX.TabControl, FMX.Controls.Presentation, FMX.Edit,
   FMX.Effects, FMX.ListView.Types, FMX.ListView.Appearances,
-  FMX.ListView.Adapters.Base, FMX.ListView;
+  FMX.ListView.Adapters.Base, FMX.ListView, FMX.StdCtrls, FMX.ScrollBox,
+  FMX.Memo, FMX.MultiView;
 
 type
   TAndroid_metas = class(TForm)
@@ -39,9 +40,83 @@ type
     ListaVaziaFundo: TRectangle;
     Image_sem_Metas: TImage;
     Seta_Sem_Metas: TImage;
+    Img_meta_concluida: TImage;
+    Img_meta_concluir: TImage;
+    Img_prioridade_alta: TImage;
+    Img_prioridade_baixa: TImage;
+    Lay_container_meta: TLayout;
+    Lay_container_status: TLayout;
+    Btn_status: TImage;
+    Label_meta: TLabel;
+    Lay_descricao_exibe: TLayout;
+    Linha_descricao_exibe: TLine;
+    Lbl_descricao_title_exibe: TLabel;
+    Lay_container_descricao_exibe: TLayout;
+    Label_descricao: TLabel;
+    Lay_prioridade_exibe: TLayout;
+    Linha_categoria_title_exibe: TLine;
+    Lbl_prioridade_exibe: TLabel;
+    Lay_container_prioridade: TLayout;
+    Lay_prioridade_btn: TLayout;
+    Fundo_btn_prioridade: TRectangle;
+    img_prioridade_btn: TImage;
+    Lbl_prioridade_btn: TLabel;
+    Btn_apaga_meta: TImage;
+    Btn_edita_meta: TImage;
+    Lay_meta: TLayout;
+    Lay_tarefa_container: TLayout;
+    Rec_tarefa: TRectangle;
+    ShadowEffect1: TShadowEffect;
+    Img_tarefa: TImage;
+    Edit_meta: TEdit;
+    Lay_descricao_edit: TLayout;
+    Linha_descricao_edit: TLine;
+    Label_descricao_edit: TLabel;
+    Lay_descricao_container: TLayout;
+    Lay_descricao_edit_cont: TLayout;
+    Rect_descricao: TRectangle;
+    sombra_descrcao_container: TShadowEffect;
+    Ed_descricao: TMemo;
+    Lay_prioridade_edit: TLayout;
+    Linha_categoria_edit: TLine;
+    Label_categoria_edit: TLabel;
+    Lay_prioridade_container: TLayout;
+    Btn_OK: TImage;
+    Lay_prioridade_alta: TLayout;
+    Btn_prioridade_alta: TRectangle;
+    Img_btn_prioridade_alta: TImage;
+    Lbl_prioridade_btn_alta: TLabel;
+    Img_select_Alta: TImage;
+    Lay_prioridade_normal: TLayout;
+    btn_prioridade_normal: TRectangle;
+    Label2: TLabel;
+    Img_select_Normal: TImage;
+    Lay_prioridade_baixa: TLayout;
+    btn_prioridade_baixa: TRectangle;
+    Img_baixa_prioridade: TImage;
+    Lbl_prioridade_baixa: TLabel;
+    Img_select_Baixa: TImage;
+    Menu_filtros: TMultiView;
+    fundo_menu_filtro: TRectangle;
+    ShadowEffect3: TShadowEffect;
+    Menu_sem_filtro: TLayout;
+    Menu_Prioridade_Alta: TLayout;
+    Menu_Prioridade_normal: TLayout;
+    Menu_prioridade_Baixa: TLayout;
+    Img_menu_prioridade_alta: TImage;
+    lbl_menu_prioridade_alta: TLabel;
+    lbl_sem_filtro: TLabel;
+    lbl_prioridade_menu_normal: TLabel;
+    Seletor_filtro: TImage;
+    Img_menu_prioridade_baixa: TImage;
+    lbl_enu_prioridade_baixa: TLabel;
     procedure AnimaStatusFinish(Sender: TObject);
     procedure Botao_voltarClick(Sender: TObject);
     procedure FormShow(Sender: TObject);
+    procedure Menu_sem_filtroClick(Sender: TObject);
+    procedure Menu_Prioridade_AltaClick(Sender: TObject);
+    procedure Menu_Prioridade_normalClick(Sender: TObject);
+    procedure Menu_prioridade_BaixaClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -76,6 +151,30 @@ end;
 procedure TAndroid_metas.FormShow(Sender: TObject);
 begin
   AnimaStatus.Start;
+end;
+
+procedure TAndroid_metas.Menu_Prioridade_AltaClick(Sender: TObject);
+begin
+  Seletor_filtro.Parent := Menu_Prioridade_Alta;
+  Menu_filtros.HideMaster;
+end;
+
+procedure TAndroid_metas.Menu_prioridade_BaixaClick(Sender: TObject);
+begin
+  Seletor_filtro.Parent := Menu_prioridade_Baixa;
+  Menu_filtros.HideMaster;
+end;
+
+procedure TAndroid_metas.Menu_Prioridade_normalClick(Sender: TObject);
+begin
+  Seletor_filtro.Parent := Menu_Prioridade_normal;
+  Menu_filtros.HideMaster;
+end;
+
+procedure TAndroid_metas.Menu_sem_filtroClick(Sender: TObject);
+begin
+  Seletor_filtro.Parent := Menu_sem_filtro;
+  Menu_filtros.HideMaster;
 end;
 
 end.
