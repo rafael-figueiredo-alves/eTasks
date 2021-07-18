9d9f02d7e548edef78bd0d5603a5464a67518628
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2021-07-18 20:25:55 -0300

    Update 17/07/2021
    Atualização que visa trabalhar com o sistema de ajuda. Ainda há muito para terminar.

--- Source/View/Android/eTasks.View.Android.help.pas
+++ Source/View/Android/eTasks.View.Android.help.pas
@@ -30,8 +30,6 @@ type
     title_Ajuda: TImage;
     RecStatus: TRectangle;
     AnimaStatus: TFloatAnimation;
-    Label_quantidade_edit: TLabel;
-    Line1: TLine;
     TabPrincipal: TTabControl;
     TabHome: TTabItem;
     TabTarefas: TTabItem;
@@ -40,9 +38,59 @@ type
     TabListas: TTabItem;
     TabContas: TTabItem;
     TabAtualizacao: TTabItem;
+    Btn_Avancar_Pagina: TImage;
+    Btn_Home: TImage;
+    Btn_Voltar_pagina: TImage;
+    TabNovidades: TTabItem;
+    TabPerguntas: TTabItem;
+    Lay_banner_inferior: TLayout;
+    Banner_ajuda: TImage;
+    Lay_pergunta_top: TLayout;
+    Img_pergunta_menu: TImage;
+    Lay_Novidades_btn: TLayout;
+    Lay_Tarefas_btn: TLayout;
+    Lay_Categorias_btn: TLayout;
+    Lay_Objetivos_btn: TLayout;
+    Lay_compras_btn: TLayout;
+    Lay_usuarios_btn: TLayout;
+    Lay_atualiazacoes_btn: TLayout;
+    Lay_perguntas_btn: TLayout;
+    Image_novidades: TImage;
+    Image_tarefas: TImage;
+    Image_categorias: TImage;
+    Image_Objetivos: TImage;
+    Image_compras: TImage;
+    Image_usuario: TImage;
+    Image_updates: TImage;
+    Image_perguntas: TImage;
+    AnimaVoltaPagina: TFloatAnimation;
+    AnimaHome: TFloatAnimation;
+    AnimaAvancaPagina: TFloatAnimation;
+    NavTarefas: TTabControl;
+    NavTarefasHome: TTabItem;
+    Layout1: TLayout;
+    Image1: TImage;
+    NavTarefasNova: TTabItem;
+    LayBanner_novidades: TLayout;
+    Banner_novidades: TImage;
+    Lay_Novidades_texto: TLayout;
+    Image_novidades_texto: TImage;
     procedure AnimaStatusFinish(Sender: TObject);
     procedure Botao_voltarClick(Sender: TObject);
     procedure FormShow(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure TabPrincipalChange(Sender: TObject);
+    procedure Lay_Novidades_btnClick(Sender: TObject);
+    procedure Lay_Tarefas_btnClick(Sender: TObject);
+    procedure Lay_Categorias_btnClick(Sender: TObject);
+    procedure Lay_Objetivos_btnClick(Sender: TObject);
+    procedure Lay_compras_btnClick(Sender: TObject);
+    procedure Lay_usuarios_btnClick(Sender: TObject);
+    procedure Lay_atualiazacoes_btnClick(Sender: TObject);
+    procedure Lay_perguntas_btnClick(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure FormKeyUp(Sender: TObject; var Key: Word; var KeyChar: Char;
+      Shift: TShiftState);
   private
     { Private declarations }
   public
@@ -53,6 +101,10 @@ var
   Form_Android_Ajuda: TForm_Android_Ajuda;
 
 implementation
+
+uses
+  FMX.Platform,
+  FMX.VirtualKeyBoard;
 
 {$R *.fmx}
 
@@ -71,12 +123,112 @@ end;
 
 procedure TForm_Android_Ajuda.Botao_voltarClick(Sender: TObject);
 begin
-  AnimaStatus.Start;
+  if TabPrincipal.ActiveTab = TabHome then
+   AnimaStatus.Start
+  else
+   TabPrincipal.GotoVisibleTab(TabHome.Index);
+end;
+
+procedure TForm_Android_Ajuda.FormClose(Sender: TObject;
+  var Action: TCloseAction);
+begin
+  Action := TCloseAction.caFree;
+  Form_Android_Ajuda := nil;
+end;
+
+procedure TForm_Android_Ajuda.FormCreate(Sender: TObject);
+begin
+  Btn_Voltar_pagina.Visible := false;
+  Btn_Avancar_Pagina.Visible := False;
+  Btn_Home.Visible := false;
+end;
+
+procedure TForm_Android_Ajuda.FormKeyUp(Sender: TObject; var Key: Word;
+  var KeyChar: Char; Shift: TShiftState);
+Var
+  FService: iFMXVirtualKeyboardService;
+begin
+  if (Key = vkHardwareBack) then
+  begin
+    TPlatformServices.Current.SupportsPlatformService
+      (iFMXVirtualKeyboardService, IInterface(FService));
+    if (FService <> Nil) and (TVirtualKeyboardState.Visible
+      in FService.VirtualKeyboardState) then
+    begin
+      // Botão BACK pressionado e teclado vísivel, apenas fecha o teclado
+    end
+    else
+     begin
+       Key := 0;
+       Botao_voltarClick(sender);
+     end;
+  end;
 end;
 
 procedure TForm_Android_Ajuda.FormShow(Sender: TObject);
 begin
   AnimaStatus.Start;
+  Btn_Voltar_pagina.Visible := false;
+  Btn_Avancar_Pagina.Visible := False;
+  Btn_Home.Visible := false;
+end;
+
+procedure TForm_Android_Ajuda.Lay_atualiazacoes_btnClick(Sender: TObject);
+begin
+  TabPrincipal.GotoVisibleTab(TabAtualizacao.Index);
+end;
+
+procedure TForm_Android_Ajuda.Lay_Categorias_btnClick(Sender: TObject);
+begin
+  TabPrincipal.GotoVisibleTab(TabCategorias.Index);
+end;
+
+procedure TForm_Android_Ajuda.Lay_compras_btnClick(Sender: TObject);
+begin
+  TabPrincipal.GotoVisibleTab(TabListas.Index);
+end;
+
+procedure TForm_Android_Ajuda.Lay_Novidades_btnClick(Sender: TObject);
+begin
+  TabPrincipal.GotoVisibleTab(TabNovidades.Index);
+end;
+
+procedure TForm_Android_Ajuda.Lay_Objetivos_btnClick(Sender: TObject);
+begin
+ TabPrincipal.GotoVisibleTab(TabMetas.Index);
+end;
+
+procedure TForm_Android_Ajuda.Lay_perguntas_btnClick(Sender: TObject);
+begin
+  TabPrincipal.GotoVisibleTab(TabPerguntas.Index);
+end;
+
+procedure TForm_Android_Ajuda.Lay_Tarefas_btnClick(Sender: TObject);
+begin
+  TabPrincipal.GotoVisibleTab(TabTarefas.Index);
+end;
+
+procedure TForm_Android_Ajuda.Lay_usuarios_btnClick(Sender: TObject);
+begin
+  TabPrincipal.GotoVisibleTab(TabContas.Index);
+end;
+
+procedure TForm_Android_Ajuda.TabPrincipalChange(Sender: TObject);
+begin
+ case TabPrincipal.TabIndex of
+  0: begin
+      Btn_Voltar_pagina.Visible := false;
+      Btn_Home.Visible := false;
+      Btn_Avancar_Pagina.Visible := False;
+     end;
+ else
+  begin
+   Btn_Avancar_Pagina.Visible := True;
+   Btn_Home.Visible := True;
+   Btn_Voltar_pagina.Enabled := false;
+   Btn_Voltar_pagina.Visible := True;
+  end;
+  end;
 end;
 
 end.
--- Source/View/Windows/eTasks.View.Windows.help.pas
+++ Source/View/Windows/eTasks.View.Windows.help.pas
@@ -18,28 +18,73 @@ uses
   FMX.StdCtrls,
   FMX.Controls.Presentation,
   FMX.Effects,
-  FMX.TabControl;
+  FMX.TabControl, FMX.Ani;
 
 type
   TForm_Windows_Ajuda = class(TForm)
     Lay_main: TLayout;
     StyleBook1: TStyleBook;
-    TabPrincipal: TTabControl;
-    TabHome: TTabItem;
-    TabTarefas: TTabItem;
-    TabMetas: TTabItem;
-    TabCategorias: TTabItem;
-    TabListas: TTabItem;
-    TabContas: TTabItem;
-    TabAtualizacao: TTabItem;
     ToolBar_container: TLayout;
     ToolBar: TLayout;
     Linha_titulo: TLine;
     Botao_voltar: TImage;
     title_Ajuda: TImage;
-    Label_quantidade_edit: TLabel;
-    Line1: TLine;
+    TabPrincipal: TTabControl;
+    TabHome: TTabItem;
+    Lay_banner_inferior: TLayout;
+    Banner_ajuda: TImage;
+    Lay_pergunta_top: TLayout;
+    Img_pergunta_menu: TImage;
+    Lay_Novidades_btn: TLayout;
+    Image_novidades: TImage;
+    Lay_Tarefas_btn: TLayout;
+    Image_tarefas: TImage;
+    Lay_Categorias_btn: TLayout;
+    Image_categorias: TImage;
+    Lay_Objetivos_btn: TLayout;
+    Image_Objetivos: TImage;
+    Lay_compras_btn: TLayout;
+    Image_compras: TImage;
+    Lay_usuarios_btn: TLayout;
+    Image_usuario: TImage;
+    Lay_atualiazacoes_btn: TLayout;
+    Image_updates: TImage;
+    Lay_perguntas_btn: TLayout;
+    Image_perguntas: TImage;
+    TabNovidades: TTabItem;
+    TabTarefas: TTabItem;
+    TabCategorias: TTabItem;
+    TabMetas: TTabItem;
+    TabListas: TTabItem;
+    TabContas: TTabItem;
+    TabAtualizacao: TTabItem;
+    TabPerguntas: TTabItem;
+    Btn_Avancar_Pagina: TImage;
+    Btn_Home: TImage;
+    Btn_Voltar_pagina: TImage;
+    AnimaBtnVolta: TFloatAnimation;
+    AnimaHome: TFloatAnimation;
+    AnimaAvancaBtn: TFloatAnimation;
+    LayBanner_novidades: TLayout;
+    Banner_novidades: TImage;
+    NavTarefas: TTabControl;
+    NavTarefasHome: TTabItem;
+    NavTarefasNova: TTabItem;
+    Layout1: TLayout;
+    Image1: TImage;
+    Lay_Novidades_texto: TLayout;
+    Image_novidades_texto: TImage;
     procedure Btn_voltarClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure Lay_Novidades_btnClick(Sender: TObject);
+    procedure Lay_Tarefas_btnClick(Sender: TObject);
+    procedure Lay_Categorias_btnClick(Sender: TObject);
+    procedure Lay_Objetivos_btnClick(Sender: TObject);
+    procedure Lay_compras_btnClick(Sender: TObject);
+    procedure Lay_usuarios_btnClick(Sender: TObject);
+    procedure Lay_atualiazacoes_btnClick(Sender: TObject);
+    procedure Lay_perguntas_btnClick(Sender: TObject);
+    procedure TabPrincipalChange(Sender: TObject);
   private
     { Private declarations }
     FBtnVoltarClick : TProc;
@@ -66,12 +111,80 @@ end;
 
 procedure TForm_Windows_Ajuda.Btn_voltarClick(Sender: TObject);
 begin
-  FBtnVoltarClick;
+  if TabPrincipal.ActiveTab = TabHome then
+   FBtnVoltarClick
+  else
+   TabPrincipal.GotoVisibleTab(TabHome.Index);
 end;
 
 function TForm_Windows_Ajuda.Exibir: TLayout;
 begin
   Result := Lay_main;
+end;
+
+procedure TForm_Windows_Ajuda.FormCreate(Sender: TObject);
+begin
+  Btn_Voltar_pagina.Visible := false;
+  Btn_Avancar_Pagina.Visible := False;
+  Btn_Home.Visible := false;
+end;
+
+procedure TForm_Windows_Ajuda.Lay_atualiazacoes_btnClick(Sender: TObject);
+begin
+  TabPrincipal.GotoVisibleTab(TabAtualizacao.Index);
+end;
+
+procedure TForm_Windows_Ajuda.Lay_Categorias_btnClick(Sender: TObject);
+begin
+  TabPrincipal.GotoVisibleTab(TabCategorias.Index);
+end;
+
+procedure TForm_Windows_Ajuda.Lay_compras_btnClick(Sender: TObject);
+begin
+  TabPrincipal.GotoVisibleTab(TabListas.Index);
+end;
+
+procedure TForm_Windows_Ajuda.Lay_Novidades_btnClick(Sender: TObject);
+begin
+  TabPrincipal.GotoVisibleTab(TabNovidades.Index);
+end;
+
+procedure TForm_Windows_Ajuda.Lay_Objetivos_btnClick(Sender: TObject);
+begin
+  TabPrincipal.GotoVisibleTab(TabMetas.Index);
+end;
+
+procedure TForm_Windows_Ajuda.Lay_perguntas_btnClick(Sender: TObject);
+begin
+  TabPrincipal.GotoVisibleTab(TabPerguntas.Index);
+end;
+
+procedure TForm_Windows_Ajuda.Lay_Tarefas_btnClick(Sender: TObject);
+begin
+  TabPrincipal.GotoVisibleTab(TabTarefas.Index);
+end;
+
+procedure TForm_Windows_Ajuda.Lay_usuarios_btnClick(Sender: TObject);
+begin
+  TabPrincipal.GotoVisibleTab(TabContas.Index);
+end;
+
+procedure TForm_Windows_Ajuda.TabPrincipalChange(Sender: TObject);
+begin
+ case TabPrincipal.TabIndex of
+  0: begin
+      Btn_Voltar_pagina.Visible := false;
+      Btn_Home.Visible := false;
+      Btn_Avancar_Pagina.Visible := False;
+     end;
+ else
+  begin
+   Btn_Avancar_Pagina.Visible := True;
+   Btn_Home.Visible := True;
+   Btn_Voltar_pagina.Enabled := false;
+   Btn_Voltar_pagina.Visible := True;
+  end;
+  end;
 end;
 
 end.
