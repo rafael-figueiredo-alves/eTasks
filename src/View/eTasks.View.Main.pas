2acead3eb9d07e38eee6b212df5506b81beffba5
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2024-09-10 21:20:51 -0300

    Prototyping tests on Delphi

--- src/View/eTasks.View.Main.pas
+++ src/View/eTasks.View.Main.pas
@@ -5,7 +5,7 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Effects,
-  FMX.Objects, FMX.Controls.Presentation, FMX.StdCtrls;
+  FMX.Objects, FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts;
 
 type
   TfMain = class(TForm)
@@ -16,6 +16,12 @@ type
     Circle1: TCircle;
     Image2: TImage;
     Image3: TImage;
+    Rectangle2: TRectangle;
+    Label2: TLabel;
+    Layout1: TLayout;
+    Rectangle3: TRectangle;
+    Label3: TLabel;
+    procedure FormResize(Sender: TObject);
   private
     { Private declarations }
   public
@@ -28,5 +34,19 @@ var
 implementation
 
 {$R *.fmx}
+
+procedure TfMain.FormResize(Sender: TObject);
+begin
+  if(fMain.Width <= 768)then
+   begin
+     Rectangle2.Visible := false;
+     Layout1.Visible := true;
+   end
+  else
+   begin
+     Rectangle2.Visible := true;
+     Layout1.Visible := false;
+   end;
+end;
 
 end.
